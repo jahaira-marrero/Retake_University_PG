a3b520d636944322ac4228ae50f1c7cb57cf34be
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-02-13 12:21:15 -0500

    Time for Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :office_hours
-  resources :registrars
-  resources :instructors
-  resources :courses
-  resources :students
+  # resources :office_hours
+
+  
+  # resources :registrars
+  # resources :instructors
+  # resources :courses
+  # resources :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
